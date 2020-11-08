17e6c6c1b57ec374099f90353702540c2abc0916
kylymbek1 <kylymbekbakytbekov1@gmail.com>
2020-11-09 01:33:50 +0600

    2 урок. Роутинг запросов

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
-def posts_list(request):
+
+def hello(request):
     return HttpResponse('<h1> Hello World </h1>')
