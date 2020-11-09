2b988be90506617726af48891df67d51d925b217
kylymbek1 <kylymbekbakytbekov1@gmail.com>
2020-11-09 15:14:08 +0600

    5 урок. Создание модели Tag, отношения ManyToMany, includes

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
 
@@ -15,3 +16,15 @@ class Post(models.Model):
     
     def __str__(self):
         return '{}'.format(self.title)
+
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.title)    
