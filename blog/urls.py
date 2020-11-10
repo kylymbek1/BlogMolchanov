9e2c579b00d42f972942ae9059f2c50d2cca3008
kylymbek1 <kylymbekbakytbekov1@gmail.com>
2020-11-10 12:33:39 +0600

    7 урок. Работа с формами Form, ModelForm, валидация данных

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', posts_list, name='posts_list_url'),
     path('post/<slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
-    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url')
+    path('tag/create', TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url'),
+
+
 ]
 
